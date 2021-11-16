fd0119bcc67e82ffce9ac1d10c33640193125fb8
Dhruv3872 <37532930+Dhruv3872@users.noreply.github.com>
2021-11-16 20:02:46 +0530

    Deleted the unused folder.

--- Other files/demo.php
+++
@@ -1,8 +0,0 @@
-<?php
-require 'vendor/autoload.php';
-//$client = new MongoDB\Client("mongodb://localhost:27017");
-$client = new MongoDB\Client;
-$companydb = $client->companydb;
-$result1 = $companydb->createCollection('emp1');
-var_dump($result1);
-?>
